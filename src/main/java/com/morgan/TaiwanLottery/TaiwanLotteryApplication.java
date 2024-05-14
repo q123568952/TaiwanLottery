386f737e5ad9045ee31b4385bf93541473e7ccb0
Morgan <m800313@gmail.com>
2024-05-14 21:48:35 +0800

    first commit of lottery project

---
+++ src/main/java/com/morgan/TaiwanLottery/TaiwanLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.morgan.TaiwanLottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiwanLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaiwanLotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morgan/TaiwanLottery/TaiwanLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morgan.TaiwanLottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaiwanLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
