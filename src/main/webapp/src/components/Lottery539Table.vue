4857185ff533e345be29c5d5d0e0be36729af3fe
Morgan <m800313@gmail.com>
2024-10-05 22:02:49 +0800

    add select and input

--- src/main/webapp/src/components/Lottery539Table.vue
+++ src/main/webapp/src/components/Lottery539Table.vue
@@ -1,11 +1,34 @@
 <template>
+  <div class="mt-4">
+    <el-input
+      v-model="input3"
+      style="max-width: 600px "
+      placeholder="請輸入號碼，EX: 01,02,03"
+      class="input-with-select"
+    >
+      <template #prepend>
+        <el-select v-model="select" placeholder="分析方法" style="width: 115px">
+          <el-option label="分析方法一" value="1" />
+          <el-option label="分析方法二" value="2" />
+          <el-option label="分析方法三" value="3" />
+        </el-select>
+      </template>
+      <template #append>
+        <el-button :icon="Search" />
+      </template>
+    </el-input>
+  </div>
+  
   <el-table :data="tableData" stripe style="width: 100%">
     <el-table-column prop="lotterytime" label="開獎時間"  />
     <el-table-column prop="lotterynumbers" label="開獎號碼"  />
   </el-table>
 </template>
-
+<script setup>
+import { Search } from '@element-plus/icons-vue';
+</script>
 <script>
+
    export default {    
     data() {
       return {
@@ -16,6 +39,8 @@
         queryString: null,
       },    
       tableData: [],     
+      select : "",
+      input3: ""
       }  
     },
     //VUE初始化執行
